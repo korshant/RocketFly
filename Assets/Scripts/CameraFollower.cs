afdee5c42de93721818304507d644c0dbd96f1e3
korshant <a.korshunov@hqsoftwarelabs.com>
2023-02-03 10:25:43 +0300

    Add toogle to CameraFollower

--- Assets/Scripts/CameraFollower.cs
+++ Assets/Scripts/CameraFollower.cs
@@ -5,10 +5,22 @@ public class CameraFollower : MonoBehaviour
     [SerializeField] private Transform target;
     [SerializeField] private float smoothSpeed = 0.125f;
     [SerializeField] private Vector3 offset;
+
+    private bool _isEnabled = true;
+
+    public bool IsEnabled
+    {
+        get => _isEnabled;
+        set => _isEnabled = value;
+    }
+
     private void LateUpdate()
     {
-        Vector3 desiredPosition = target.position + offset;
-        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
-        transform.position = new Vector3(transform.position.x, smoothedPosition.y, transform.position.z);
+        if (_isEnabled)
+        {
+            Vector3 desiredPosition = target.position + offset;
+            Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
+            transform.position = new Vector3(transform.position.x, smoothedPosition.y, transform.position.z);
+        }
     }
 }
